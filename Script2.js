aa665482e9079e418cffdfc2bfa7cef878be37ea
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 00:56:09 +0530

    Evaluated expression left to right

--- Script2.js
+++ Script2.js
@@ -3,3 +3,9 @@ let answ= 23 + " Hello";
 console.log(answ);
 asnw= "23" + " Hello";
 console.log(answ);
+
+// left two right evaluation 
+let bucket1= 34;
+let bucket2= 56;
+let total= bucket1 + bucket2 + " points";
+console.log(total);

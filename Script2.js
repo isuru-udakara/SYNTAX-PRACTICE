0a1ca15f7e1695ef09df96f10b85d10b24bff546
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 00:59:36 +0530

    Evaluated expression left to right

--- Script2.js
+++ Script2.js
@@ -9,3 +9,9 @@ let bucket1= 34;
 let bucket2= 56;
 let total= bucket1 + bucket2 + " points";
 console.log(total);
+
+let firt_name= "kelvin";
+let registration_id= 789;
+let birthdate= 23421;
+let username= firt_name + registration_id + birthdate;
+console.log(username);

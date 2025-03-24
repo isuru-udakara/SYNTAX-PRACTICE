611a1e10e11563748b2668b89b098b97f0aee234
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 01:09:25 +0530

    Created two double variables with exponential notation

--- Script3.js
+++ Script3.js
@@ -8,3 +8,9 @@ console.log("Output voltage: "+ output_voltage);
 
 let cname= "Gotham";
 console.log("Name: "+ cname);
+
+// exponential notation 
+let someones_constant1= 6.22e14;
+let someones_constant2= 45e-10;
+console.log(someones_constant1);
+console.log(someones_constant2);

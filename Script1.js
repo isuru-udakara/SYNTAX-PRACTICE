ec6322255527282c11ab8f42abcc91ff3948c044
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 00:44:13 +0530

    Created two variables using const and print

--- Script1.js
+++ Script1.js
@@ -9,3 +9,9 @@ let value3= 45;
 let value4= 24;
 let sub= value3 - value4; 
 console.log("Subtraction: "+ sub);
+
+// creating const variables 
+const PI= 3.142;
+const number= 2;
+const Param= PI * number;
+console.log("Constant: "+ Param);

159c19dd8a94066c445811d2337abdc0fb3fceb8
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 01:05:23 +0530

    Created string variable

--- Script3.js
+++ Script3.js
@@ -5,3 +5,6 @@ console.log("Sensor Result: "+ outdoor_sensor);
 
 let output_voltage= 3.12422;
 console.log("Output voltage: "+ output_voltage);
+
+let cname= "Gotham";
+console.log("Name: "+ cname);

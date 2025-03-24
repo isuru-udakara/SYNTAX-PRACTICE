52686867a09744a93cc56c2a0fa8b8f8e9a84781
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 00:36:55 +0530

    Created two variables using let and calculated difference

--- Script1.js
+++ Script1.js
@@ -3,3 +3,9 @@ var value1= 25;
 var value2= 89;
 var sum= value1 + value2;
 console.log("Sum: "+ sum);
+
+// creating variables with let 
+let value3= 45;
+let value4= 24;
+let sub= value3 - value4; 
+console.log("Subtraction: "+ sub);

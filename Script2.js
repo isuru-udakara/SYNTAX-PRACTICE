7ef3ecf657f73ad645172c26aaa835dd4d0f2134
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 15:39:04 +0530

    Accessed properties of Human object

--- Script2.js
+++ Script2.js
@@ -21,3 +21,9 @@ Human.age= 27;
 Human.skin_tone= "brown";
 Human.height= 6.5;
 Human.weight= 69;
+
+// accessing object properties 
+console.log("Gender"+ Human.gender);
+console.log("Age: "+ Human.age);
+console.log("Skin Tone: "+ Human.skin_tone);
+console.log("Height: "+ Human.height);

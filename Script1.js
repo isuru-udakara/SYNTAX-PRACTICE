cec0879fd308876b0bb3306a227978a62cfc1bab
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 21:32:51 +0530

    Created a for loop to calcualte sum of factorial(n) and factorial(n-1)

--- Script1.js
+++ Script1.js
@@ -2,3 +2,17 @@
 for(let i= 0; i < 10; i++){
     console.log("This "+ i);
 }
+
+function factorial(n){
+    if(n == 1 || n == 0){
+        return 1;
+    }else{
+        return n * factorial(n - 1);
+    }
+}
+
+let sum= 0;
+for(let i= 2; i < 10; i++){
+    sum += (factorial(i) + factorial(i - 1));
+}
+console.log("Sum: "+ sum);

bd5c650b59d6910dcd7e1fc4397328d48b549c03
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 21:56:30 +0530

    Created an object with messages that return from functions

--- Script1.js
+++ Script1.js
@@ -3,3 +3,18 @@ const ports= [3993, 9933, 8233, 9033, 5300];
 for(let prt in ports){
     console.log("Available porst: "+ ports[prt]);
 }
+
+const msgs= {
+    day1: function(){
+        return "Hello, I want to talk to you.";
+    },
+    day2: function(){
+        return "Hey, are you there?";
+    },
+    day3: function(){
+        return "This is not what I expected.";
+    },
+    day4: function(){
+        return "You have to stop this.";
+    }
+}

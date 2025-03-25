c04d099feb651a575b0215897d06a25436479dfd
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 21:07:47 +0530

    Created a switch-case statement

--- Script1.js
+++ Script1.js
@@ -1,1 +1,23 @@
-//
+// practice swith statements 
+
+let grade= "A";
+
+switch(grade){
+    case "A":
+        console.log("Ant");
+        break;
+    case "B":
+        console.log("Bear");
+        break;
+    case "C":
+        console.log("Crane");
+        break;
+    case "D":
+        console.log("Dragon");
+        break;
+    case "E":
+        console.log("Elephant");
+        break;
+    default: 
+        console.log("Tiger");
+}

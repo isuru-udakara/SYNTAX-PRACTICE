d63987a4fb537259fa6d4e86ae0f73afd27bb50d
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 14:59:01 +0530

    Created a function to calculate sum of two numbers

--- Script1.js
+++ Script1.js
@@ -4,3 +4,10 @@ function printmyname(name){
 
 printmyname("david");
 printmyname("holand");
+
+function PrintSum(a, b){
+    return a + b;
+}
+
+console.log("Sum: "+ PrintSum(23, 89));
+console.log("Sum: "+ PrintSum(34, 67));

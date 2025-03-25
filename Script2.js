7cf96f1c72afb5d85186de3a89b42bd1d3f86739
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 15:30:12 +0530

    Created a new object using new keyword and added properties

--- Script2.js
+++ Script2.js
@@ -13,3 +13,11 @@ Computer.SSD= 500;
 Computer.Brand= "Lenovo";
 Computer.OS= "Windows";
 Computer.CPU= "Inter i5";
+
+// using new keyword 
+const Human= new Object();
+Human.gender= "Female";
+Human.age= 27;
+Human.skin_tone= "brown";
+Human.height= 6.5;
+Human.weight= 69;

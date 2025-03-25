777e60ebe28814d3e7dfba23b2cad9980f65abc0
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 16:16:12 +0530

    Completed MenuBar object

--- Script5.js
+++ Script5.js
@@ -6,6 +6,28 @@ const MenuBar= {
             item1_2: "New Folder",
             item1_3: "New Window",
             itme1_4: "New WorkSpace"
-        }
+        },
+        item2: {
+            item2_1: "Open File",
+            item2_2: "Open Folder",
+            item2_3: "Open WorkSpace"
+        },
+        item3: "Save",
+        item4: "Save As",
+        item4: "Export",
+        item5: "Import",
+        item6: "Exit"
+    },
+    Edit: {
+        item1: "Undo",
+        item2: "Redo",
+        item3: "Replace",
+        item4: "Find",
+        item5: "Go to"
+    },
+    View: {
+        item1: "FullScreen",
+        item2: "Maximize",
+        item3: "Grid"
     }
-}
+};

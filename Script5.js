55d5cce6bc81d4854b238315e53f75cbdebabfc5
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 16:11:15 +0530

    Created object menubar

--- Script5.js
+++ Script5.js
@@ -1,1 +1,11 @@
 // nested objec
+const MenuBar= {
+    File: {
+        item1: {
+            item1_1: "New File",
+            item1_2: "New Folder",
+            item1_3: "New Window",
+            itme1_4: "New WorkSpace"
+        }
+    }
+}

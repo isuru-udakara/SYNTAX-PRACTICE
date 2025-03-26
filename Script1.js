4fe85984c73f4bff6b0ab357e4d60e297e747417
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-26 16:41:50 +0530

    Created a callback function to greet

--- Script1.js
+++ Script1.js
@@ -1,1 +1,11 @@
 // callback functions 
+function Greet(name, callback){
+    console.log("Hello "+ name);
+    callback();
+}
+
+function sayBye(){
+    console.log("Goodbye !");
+}
+
+Greet("Isuru", sayBye);

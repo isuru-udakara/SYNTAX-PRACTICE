6cbf6e231f25429229907aec995f0dc82c54ef5a
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-27 08:30:16 +0530

    Created an arrow function to calculate distance

--- Script2.js
+++ Script2.js
@@ -3,3 +3,10 @@ const Force= (mass, acceleration) => {
 }
 Force(4000, 7);
 Force(4590, 8.67);
+
+// s= v + at
+const Distance= (velocity, acceleration, time) => {
+    console.log("Distance: "+ (velocity + (acceleration * time)));
+}
+
+Distance(2, 0.6, 20);

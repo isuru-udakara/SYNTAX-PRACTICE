8bac33d1dbe3706d28649eee958b73d764e30045
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 21:00:16 +0530

    Created callback function to greet with name

--- Script1.js
+++ Script1.js
@@ -7,3 +7,14 @@ setTimeout(PrintMe, 2000);
 setTimeout(() => {
     console.log("This is me.!");
 }, 500);
+
+function Greet(name, callback){
+    console.log(`Hello ${name}`);
+    callback();
+}
+
+function Message(){
+    console.log("Hope, see you again!");
+}
+
+Greet("Isuru", Message);

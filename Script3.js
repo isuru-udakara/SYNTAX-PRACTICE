5f4db738649fb210a2136d2395ca05d8eba6e25a
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 23:22:41 +0530

    Created a method to check two side of equation with values

--- Script3.js
+++ Script3.js
@@ -10,3 +10,19 @@ console.log(force(23, 2.1));
 
 const Concentration= (mole, volume) => Math.round((mole/volume) * 100)/100;
 console.log(`Concentration: ${Concentration(3.4, 300)} molem-3`);
+
+
+// check pv = nRT
+function check(part1, part2){
+    if(part1(2e5, 3e3) == part2(3e-2, 400)){
+        console.log("Equation is balanced.");
+    }else{
+        console.log("Equation is not balanced.");
+    }
+}
+
+check((p, v) => {
+    return (p * v).toFixed(2);
+}, (n, T) => {
+    return (n * 3.233e4 * T).toFixed(2);
+});

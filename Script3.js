3c98621245a068e8c7e6ec319abc84f2144f15b5
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 23:13:02 +0530

    Created an arrow function to calculate concentraion

--- Script3.js
+++ Script3.js
@@ -7,3 +7,6 @@ const force= (mass, acceleration) => {
         return err;
     }};
 console.log(force(23, 2.1));
+
+const Concentration= (mole, volume) => Math.round((mole/volume) * 100)/100;
+console.log(`Concentration: ${Concentration(3.4, 300)} molem-3`);

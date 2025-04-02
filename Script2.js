2293cd013ad83995911833d8c8860da547e61960
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 22:55:25 +0530

    Created a callback funciton to calculate square of numbers in an array

--- Script2.js
+++ Script2.js
@@ -14,3 +14,16 @@ function Concentrate(callback){
 }
 
 Concentrate((mole, volume) => {console.log(`${mole/volume}molem-3 `)});
+
+// function to process array
+function processArray(arr, callback){
+    const newArr= [];
+    for(n in arr){
+        newArr.push(callback(arr[n]));
+    }
+    return newArr;
+}
+
+const numbers= [1, 2, 3, 4, 5, 6];
+const squared = processArray(numbers, (num) => num * num);
+console.log(squared);

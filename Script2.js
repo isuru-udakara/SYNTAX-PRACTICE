4cd181936ddbfaea977cce1d9f3f4c1e43e0ef0a
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 22:40:13 +0530

    Created a callback method to calculate concentration

--- Script2.js
+++ Script2.js
@@ -5,3 +5,12 @@ function greet(callback){
 }
 
 greet((name) => {console.log(`Hello ${name}!`)});
+
+// calculate concentrate
+function Concentrate(callback){
+
+    callback(0.4, 200);
+    callback(2, 3000);
+}
+
+Concentrate((mole, volume) => {console.log(`${mole/volume}molem-3 `)});

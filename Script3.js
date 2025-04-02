64d1be26e8f606ac2679d325c1a97ceb5eb77592
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 23:01:53 +0530

    Created an arrow funciton to calcualte force

--- Script3.js
+++ Script3.js
@@ -1,1 +1,9 @@
 // callback exercises
+
+const force= (mass, acceleration) => {
+    try{
+        return mass * acceleration;
+    } catch(err){
+        return err;
+    }};
+console.log(force(23, 2.1));

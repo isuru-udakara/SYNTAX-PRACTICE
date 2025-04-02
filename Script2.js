dc2287dae9102ea009161942a638452141325084
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 21:10:00 +0530

    Created callback function to calculate square of numbers given in an array

--- Script2.js
+++ Script2.js
@@ -2,3 +2,12 @@ function square(num){
     return num * num;
 }
 
+function processArray(arr, callback){
+    const newArr= [];
+    for(n in arr){
+        newArr.push(callback(arr[n]));
+    }
+    console.log(newArr);
+}
+
+processArray([1, 2, 3], square);
